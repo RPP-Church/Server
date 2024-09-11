49fc429333ed75fb767e771e8f405bda9d66545b
Kingvics <okoromivictorsunday@gmail.com>
2024-09-12 00:50:17 +0100

    cron job

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const port = process.env.PORT || 5000;
 
 const scheduleTask = async () => {
   const task = cron.schedule(
-    '49 * * * *',
+    '51 * * * *',
     () => {
       console.log('s')
       const activityDate = new Date().toISOString()?.slice(0, 10);
